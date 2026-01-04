72db6742305debb93816ed477e6dd6a41de5ad12
ofirGer <142690409+ofirGer@users.noreply.github.com>
2026-01-04 14:42:48 +0200

    Create peer.py

---
+++ peer.py
@@ -0,0 +1,62 @@
+import socket
+import threading
+import time
+import json
+
+# Example files this peer owns
+my_files = {
+    "file1.txt": 1024,
+    "file2.mp3": 5000
+}
+
+# Table of known peers and their files
+peer_table = {}
+
+BROADCAST_PORT = 50000
+BROADCAST_INTERVAL = 5  # seconds
+
+
+def broadcast_presence():
+    """Broadcast this peer's info to the local network."""
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    while True:
+        message = json.dumps({
+            "type": "PEER",
+            "files": my_files
+        })
+        sock.sendto(message.encode(), ('<broadcast>', BROADCAST_PORT))
+        time.sleep(BROADCAST_INTERVAL)
+
+
+def listen_for_peers():
+    """Listen for broadcast messages from other peers."""
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(('', BROADCAST_PORT))
+
+    while True:
+        data, addr = sock.recvfrom(4096)
+        print("Received broadcast from:", addr)
+
+        try:
+            msg = data.decode()
+            data_obj = json.loads(msg)
+
+            if data_obj.get("type") == "PEER":
+                peer_table[addr[0]] = data_obj["files"]
+                print("Updated peer table:")
+                print(peer_table)
+
+        except Exception as e:
+            print("Failed to process message:", e)
+
+
+if __name__ == "__main__":
+    threading.Thread(target=broadcast_presence, daemon=True).start()
+    threading.Thread(target=listen_for_peers, daemon=True).start()
+
+    while True:
+        cmd = input("Enter 'table' to see known peers: ")
+        if cmd == "table":
+            print(peer_table)
