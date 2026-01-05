02ee11714614a7628d5d9096282e8b8bba8b9e45
cyber <lihibaranetz@gmail.com>
2026-01-05 10:35:08 +0200

    fixed the broadcast

--- peer.py
+++ peer.py
@@ -4,26 +4,23 @@ import time
 import json
 import uuid
 
-# Unique ID for this peer
 PEER_ID = str(uuid.uuid4())
 
-# Example files this peer owns
 my_files = {
     "file1.txt": 1024,
     "file2.mp3": 5000
 }
 
-# Discovery table
 peer_table = {}
 
 BROADCAST_PORT = 50000
-BROADCAST_INTERVAL = 5  # seconds
+BROADCAST_INTERVAL = 5
 
 
 def broadcast_presence():
-    """Broadcast this peer's info to the local network."""
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     while True:
         message = json.dumps({
@@ -31,34 +28,33 @@ def broadcast_presence():
             "peer_id": PEER_ID,
             "files": my_files
         })
-        sock.sendto(message.encode(), ('<broadcast>', BROADCAST_PORT))
+
+        sock.sendto(message.encode(), ('172.16.255.255', BROADCAST_PORT))
+        print("Broadcast sent")
         time.sleep(BROADCAST_INTERVAL)
 
 
 def listen_for_peers():
-    """Listen for broadcast messages from other peers."""
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(('', BROADCAST_PORT))
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('0.0.0.0', BROADCAST_PORT))
 
     while True:
         data, addr = sock.recvfrom(4096)
 
 
         try:
-            msg = data.decode()
-            data_obj = json.loads(msg)
+            obj = json.loads(data.decode())
 
-            if data_obj.get("type") == "PEER":
-                peer_id = data_obj["peer_id"]
+            if obj.get("type") == "PEER":
+                peer_id = obj["peer_id"]
 
-                # Ignore our own broadcasts
                 if peer_id == PEER_ID:
                     continue
-                print("Received broadcast from:", addr)
-
+                print("Received raw data from", addr)
                 peer_table[peer_id] = {
                     "ip": addr[0],
-                    "files": data_obj["files"],
+                    "files": obj["files"],
                     "last_seen": time.time()
                 }
 
@@ -66,7 +62,7 @@ def listen_for_peers():
                 print(peer_table)
 
         except Exception as e:
-            print("Failed to process message:", e)
+            print("Error:", e)
 
 
 if __name__ == "__main__":
@@ -74,6 +70,5 @@ if __name__ == "__main__":
     threading.Thread(target=listen_for_peers, daemon=True).start()
 
     while True:
-        cmd = input("Enter 'table' to see known peers: ")
-        if cmd == "table":
+        if input("Enter 't': ") == "t":
             print(peer_table)
