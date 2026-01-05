b9b46f270d505ab6b7cc7724770a07a8b74145f1
cyber <lihibaranetz@gmail.com>
2026-01-05 10:48:34 +0200

    Cleanup for offline peers.

--- peer.py
+++ peer.py
@@ -5,6 +5,7 @@ import json
 import uuid
 
 PEER_ID = str(uuid.uuid4())
+PEER_TIMEOUT = 15
 
 my_files = {
     "file1.txt": 1024,
@@ -58,16 +59,33 @@ def listen_for_peers():
                     "last_seen": time.time()
                 }
 
-                print("Updated peer table:")
-                print(peer_table)
+                #print("Updated peer table:")
+                #print(peer_table)
 
         except Exception as e:
             print("Error:", e)
+
+def cleanup_peers():
+    while True:
+        now = time.time()
+        removed = []
+
+        for peer_id, info in list(peer_table.items()):
+            if now - info["last_seen"] > PEER_TIMEOUT:
+                removed.append(peer_id)
+                del peer_table[peer_id]
+
+        if removed:
+            print("Removed inactive peers:", removed)
+
+        time.sleep(5)
+
 
 
 if __name__ == "__main__":
     threading.Thread(target=broadcast_presence, daemon=True).start()
     threading.Thread(target=listen_for_peers, daemon=True).start()
+    threading.Thread(target=cleanup_peers, daemon=True).start()
 
     while True:
         if input("Enter 't': ") == "t":
