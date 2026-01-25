c15ce37a4aed72373e5f99e67b0fc6d7ba27224b
cyber <lihibaranetz@gmail.com>
2026-01-25 15:20:21 +0200

    Added a flask page

--- web_ui.py
+++ web_ui.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, jsonify
 import threading
 import os
+import tkinter as tk
+from tkinter import filedialog
 
 
 class WebUI:
@@ -9,22 +11,38 @@ class WebUI:
         self.file_manager = file_manager
         self.tcp_client = tcp_client
         self.app = Flask(__name__)
-        self.app.secret_key = 'super_secret_key'  # Needed for flashing messages
+        self.app.secret_key = 'super_secret_key'
 
         # --- ROUTES ---
 
         @self.app.route('/')
         def index():
-            # Pass the peer table and my files to the HTML
+            # We still pass data for the first load, but JS takes over after
             return render_template('index.html',
                                    peer_table=self.peer.peer_table,
                                    my_files=self.file_manager.my_files)
 
-        @self.app.route('/add_file', methods=['POST'])
-        def add_file():
-            filepath = request.form.get('filepath')
-            if os.path.exists(filepath):
-                self.file_manager.add_file(filepath)
+        # --- NEW: API Route for JavaScript to talk to ---
+        @self.app.route('/api/data')
+        def get_data():
+            """Returns the current state of peers and files as JSON"""
+            return jsonify({
+                "peers": self.peer.peer_table,
+                "my_files": self.file_manager.my_files
+            })
+
+        @self.app.route('/select_file', methods=['POST'])
+        def select_file():
+            root = tk.Tk()
+            root.withdraw()
+            root.attributes("-topmost", True)
+
+            file_path = filedialog.askopenfilename(title="Select a file to share")
+            root.destroy()
+
+            if file_path:
+                self.file_manager.add_file(file_path)
+
             return redirect('/')
 
         @self.app.route('/download', methods=['POST'])
@@ -32,13 +50,10 @@ class WebUI:
             target_ip = request.form.get('ip')
             filename = request.form.get('filename')
 
-            # Start download in a background thread so the website doesn't freeze
-            thread = threading.Thread(target=self.tcp_client.download_file,
-                                      args=(target_ip, filename))
-            thread.start()
+            threading.Thread(target=self.tcp_client.download_file,
+                             args=(target_ip, filename)).start()
 
             return redirect('/')
 
     def run(self):
-        # We turn off the reloader because it doesn't play nice with our Peer threads
         self.app.run(host="0.0.0.0", port=8000, debug=False, use_reloader=False)
